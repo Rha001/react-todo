22cd6277c6c9ff0fe812028bfe983b8d1bd91993
Angel Romero Herrera <romero@Angels-MacBook-Pro.local>
2017-03-03 18:23:02 -0600

    showing task input

--- react-todo/src/Panel.js
+++ react-todo/src/Panel.js
@@ -5,31 +5,46 @@ class Panel extends Component {
     super(props);
 
     this.state = {
-      items: []
+      items: [],
+      addItem: false
     };
 
     this.add = this.add.bind(this);
   }
   add(){
     this.setState((oldState) =>{
-      var tmpState = oldState.items.slice();
-      tmpState.push('asd');
+      /*var tmpState = oldState.items.slice();
+      tmpState.push('asd');*/
       return {
-        items: tmpState
+        items: oldState.items,
+        addItem: true
       }
     });
-    return false;
+
   }
   render() {
+    const name = this.props.name;
     const items = this.state.items;
+    const add = this.state.addItem;
+    let addItem;
+    if(add)
+      addItem = <textarea></textarea>;
+    else
+      addItem = null;
     return (
       <div className="col-xs-4">
-        <h3>{ this.props.name }</h3>
-        <ul >
-          {items.map((item, key) => {
-            return <li key={key}>{ item }</li>;
-          })}
+        <h3>{ name }</h3>
+        <ul className="list-group">
+          { 
+            items.map((item, key) => {
+            return <li className="list-group-item justify-content-between" key={key}>
+                      { item } 
+                      <a href="#" className="pullright">x</a>
+                  </li>;
+            }) 
+          }
         </ul>
+        { addItem }
         <a href="#" onClick={this.add}>Add card...</a>
       </div>
     );
